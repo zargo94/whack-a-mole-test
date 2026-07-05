fc22ac4f31b2cfc0ef5c631acf3874fe4331a72e
Jofebrianto94 <jo_febrianto@yahoo.com>
2026-07-05 18:00:18 +0700

    fix supabase url

--- supabase-client.js
+++ supabase-client.js
@@ -1,6 +1,6 @@
 // Ganti dua value di bawah pake punya lo dari Supabase dashboard:
 // Settings -> API -> Project URL & anon public key
-const SUPABASE_URL = "https://dgcxlopvcmuagdgtxckx.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://dgcxlopvcmuagdgtxckx.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRnY3hsb3B2Y211YWdkZ3R4Y2t4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMyNDE4ODUsImV4cCI6MjA5ODgxNzg4NX0.J45Nxe9FPNPuee3h1GyyPHVSmnjjRhcufOHrm3xTnUw";
 
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
